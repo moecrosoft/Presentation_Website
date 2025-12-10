71a198c816336e390c36c2ca7dfca1916b2a9891
moecrosoft <moe.moepyesone77@gmail.com>
2025-12-10 20:19:18 +0800

    update code

--- web.py
+++ web.py
@@ -271,6 +271,7 @@ with st.container():
     left_col,right_col = st.columns(2)
     with left_col:
         img2 = Image.open('Photo_2.jpg')
+        img2 = img2.resize((300,300))
         st.image(img2,use_column_width=True,height=200)
     with right_col:
         st.subheader('However, their correlation to resale value is weaker than floor area and year of sale')
